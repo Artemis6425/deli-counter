3d556bb474c9b8b206af7c410f4a34f05e51619a
Artemis6425 <aurorarose6425@gmail.com>
2020-10-22 14:59:32 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+katz_deli = []
+def take_a_number(array, name)
+    
+    array.push("#{name}")
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+def line(array)
+    if array.length < 1
+        puts "The line is currently empty."
+    else
+        string = "The line is currently:"
+        number = 1
+        while number <= array.length
+            string << " #{number}. #{array[number-1]}"
+            number +=1
+        end
+        puts string
+    end
+end
+def now_serving(array)
+    if array.length < 1
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array.first}."
+        array.shift
+    end
+end
